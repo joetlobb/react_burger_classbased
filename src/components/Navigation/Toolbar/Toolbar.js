84b8da3878e53a11785694b6e892fbd5a48fcabf
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-11-29 04:53:10 +0700

    Adding SideDrawer, Backdrop, DrawerToggle, and Hamburger Menu Icon

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <nav>
-      ...
+    <DrawerToggle 
+    className={classes.Menu}
+    clicked={props.drawerToggleClicked}></DrawerToggle>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
     </nav>
   </header>
 )
