7e27cb6600cf9eebb0755061e3b2b3e0891ebf42
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-02 07:59:34 +0700

    Adding Checkout Container and CheckoutSummary Component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-  state = {
-    show: true
-  }
+  // state = {
+  //   show: true
+  // }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ show: false })
-    }, 2000);
-  }
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false })
+  //   }, 2000);
+  // }
 
   render() {
     return (
       <div >
         <Layout>
-          {this.state.show ? <BurgerBuilder></BurgerBuilder> : null}
+          {/* {this.state.show ? <BurgerBuilder></BurgerBuilder> : null} */}
+          <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,8 +8,8 @@ class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
   
     return nextProps.show !== this.props.show ||
-      (nextProps.show !== this.props.show &&
-        nextProps.children !== this.props.children) ||
+      // (nextProps.show !== this.props.show &&
+      //   nextProps.children !== this.props.children) ||
       (nextProps.children && nextProps.children.type.name === 'spinner')
   }
 
