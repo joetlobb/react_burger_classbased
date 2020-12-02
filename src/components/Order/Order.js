38375b9621044ffda42f81fe4d3d1b5129c144b4
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-02 20:55:28 +0700

    Navigation Links

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,7 +4,7 @@ import classes from './Order.module.css';
 
 const order = (props) => (
   <div className={classes.Order}>
-    <p>Ingredients: Sald (1)</p>
+    <p>Ingredients: Salad (1)</p>
     <p>Price: <strong>USD 5.45</strong></p>
   </div>
 )
