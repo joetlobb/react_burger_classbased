68fb5d9b24545eb4253eeb04f7ddb2fca9d75fc9
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-07 15:25:10 +0700

    Dynamically Create Inputs based on JS Config

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -52,9 +52,9 @@ class ContactData extends Component {
       deliveryMethod: {
         elementType: 'select',
         elementConfig: {
-          option: [
-            {value: 'fastest', displayValue: 'Fastest'},
-            {value: 'cheapest', displayValue: 'Cheapest'},
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
           ]
         },
         value: ''
@@ -82,17 +82,30 @@ class ContactData extends Component {
   }
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
+
     let form = (<form>
-      <Input elementType="" elementConfig="" value="" />
-      <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-      <Input inputtype="input" type="text" name="street" placeholder="Street" />
-      <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+      {formElementsArray.map(formElement => (
+        <Input
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+        />
+      ))}
       <Button btnType="Success"
         clicked={this.orderHandler}>ORDER</Button>
     </form>);
     if (this.state.loading) {
       form = <Spinner />
     }
+
     return (
       <div className={classes.ContactData}>
         <h4>Enter Your Contact Data</h4>
