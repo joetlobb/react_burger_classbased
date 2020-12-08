5b4beee6fe2472858184746d0b5a1149c76e46c9
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-08 07:54:28 +0700

    Handling Form Submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,9 +67,17 @@ class ContactData extends Component {
     event.preventDefault();
     // console.log(this.props.ingredients);
     this.setState({ loading: true });
+
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] =
+        this.state.orderForm[formElementIdentifier].value;
+    }
+
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
+      orderData: formData
     }
     axios.post('/orders.json', order)
       .then(response => {
@@ -85,15 +93,15 @@ class ContactData extends Component {
     const updatedOrderForm = {
       ...this.state.orderForm
     };
-  // will not copy deeply, object inside this will still be 
-  // the reference to the same one in the state so we need 
-  // to copy again 
+    // will not copy deeply, object inside this will still be 
+    // the reference to the same one in the state so we need 
+    // to copy again 
     const updatedFormElement = {
       ...updatedOrderForm[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({orderForm: updatedOrderForm});
+    this.setState({ orderForm: updatedOrderForm });
   }
 
   render() {
@@ -105,19 +113,21 @@ class ContactData extends Component {
       });
     }
 
-    let form = (<form>
-      {formElementsArray.map(formElement => (
-        <Input
-          key={formElement.id}
-          elementType={formElement.config.elementType}
-          elementConfig={formElement.config.elementConfig}
-          value={formElement.config.value}
-          changed={(event) => this.inputChangedHandler(event, formElement.id)}
-        />
-      ))}
-      <Button btnType="Success"
-        clicked={this.orderHandler}>ORDER</Button>
-    </form>);
+    let form = (
+      <form onSubmit={this.orderHandler}>
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+          />
+        ))}
+        <Button btnType="Success"
+          clicked={this.orderHandler}>ORDER</Button>
+      </form>
+    );
     if (this.state.loading) {
       form = <Spinner />
     }
