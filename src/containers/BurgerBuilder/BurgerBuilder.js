77548dbdce120b73ee810d8ff63ca39460daebeb
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-10 20:21:51 +0700

    Use local state to enable a button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
   state = {
     // ingredients: null,
     // totalPrice: 4,
-    purchasable: false,
+    // purchasable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -45,7 +45,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   // addIngredientHandler = (type) => {
@@ -133,7 +133,7 @@ class BurgerBuilder extends Component {
             // ingredientRemoved={this.removeIngredientHandler}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
