5bbdb27f1c868be936eb08a3c7e427474f04cb94
Joetlobb <joetlobb@Joetlobbs-MacBook-Pro.local>
2020-12-16 13:33:30 +0700

    Loggging Users Out

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -9,7 +9,7 @@ const initialState = {
 };
 
 const authStart = (state, action) => {
-  return updateObject(state, {error: null, loading: true});
+  return updateObject(state, { error: null, loading: true });
 };
 
 const authSuccess = (state, action) => {
@@ -22,7 +22,11 @@ const authSuccess = (state, action) => {
 };
 
 const authFail = (state, action) => {
-  return updateObject(state, {error: action.error, loading: false});
+  return updateObject(state, { error: action.error, loading: false });
+};
+
+const authLogout = (state, action) => {
+  return updateObject(state, { token: null, userId: null });
 };
 
 const reducer = (state = initialState, action) => {
@@ -30,6 +34,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
+    case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
     default:
       return state;
   };
